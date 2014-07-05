3f796baa73dfef320ea5185572e23617f87b5e07
rr222cy <rr222cy@student.lnu.se>
2014-07-05 15:49:50 +0200

    Returning Customer object as JSON

---
+++ TestProject2/TestProject2/Controllers/JsonController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TestProject2.Models;
+
+namespace TestProject2.Controllers
+{
+    public class JsonController : Controller
+    {
+        //
+        // GET: /Json/
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult getJson()
+        {
+            Customer obj = new Customer();
+            obj.CustomerCode = "c001";
+            return Json(obj, JsonRequestBehavior.AllowGet);
+        }
+	}
+}
---
+++ TestProject2/TestProject2/Models/Customer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TestProject2.Models
+{
+    public class Customer
+    {
+        private string _CustomerCode;
+        public string CustomerCode
+        {
+            get { return _CustomerCode; }
+            set { _CustomerCode = value; }
+        }
+    }
+}
