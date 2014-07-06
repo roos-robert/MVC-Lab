8034af4261354a1fcfda571577bdd177fe4c93fd
rr222cy <rr222cy@student.lnu.se>
2014-07-06 21:14:07 +0200

    Class for calculating next birthday

---
+++ NextBirthday/NextBirthday/Models/Birthday.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NextBirthday.Models
+{
+    public class Birthday
+    {
+        public DateTime Birthdate { get; set; }
+
+        public string Name { get; set; }
+
+        public int Age
+        {
+            get
+            {
+                return this.NextBirthdayDate.Year - this.Birthdate.Year;
+            }
+        }
+
+        public int DaysUntilNextBirthday
+        {
+            get
+            {
+                return (this.NextBirthdayDate - DateTime.Today).Days;
+            }
+        }
+
+        public DateTime NextBirthdayDate
+        {
+            get
+            {
+                var nextBirthday = new DateTime(DateTime.Today.Year,
+                    this.Birthdate.Month, this.Birthdate.Day);
+                if (nextBirthday < DateTime.Today)
+                {
+                    nextBirthday = nextBirthday.AddYears(1);
+                }
+
+                return nextBirthday;
+            }
+        }
+    }
+}
