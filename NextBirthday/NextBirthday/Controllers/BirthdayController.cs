373839f297012c8f808882e22326e4c546ac8729
rr222cy <rr222cy@student.lnu.se>
2014-07-06 21:22:45 +0200

    Birthday controller and index view

---
+++ NextBirthday/NextBirthday/Controllers/BirthdayController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NextBirthday.Controllers
+{
+    public class BirthdayController : Controller
+    {
+        //
+        // GET: /Birthday/
+        public ActionResult Index()
+        {
+            return View();
+        }
+	}
+}
