48db947eef9e315e40ef214f24e414698beda167
rr222cy <rr222cy@student.lnu.se>
2014-07-08 20:35:08 +0200

    EFRepository added

---
+++ NextBirthday/NextBirthday/Models/Repository/EFRepository.cs
@@ -0,0 +1,28 @@
+﻿using NextBirthday.Models.DataModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NextBirthday.Models.Repository
+{
+    public class EFRepository
+    {
+        private birthdatesEntities _entities = new birthdatesEntities();
+
+        public IEnumerable<Birthday> GetBirthdays()
+        {
+            return _entities.Birthdays.ToList();
+        }
+
+        public void InsertBirthday(Birthday birthday)
+        {
+            _entities.Birthdays.Add(birthday);
+        }
+
+        public void Save()
+        {
+            _entities.SaveChanges();
+        }
+    }
+}
