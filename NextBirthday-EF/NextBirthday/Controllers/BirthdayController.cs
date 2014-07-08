84ebdcf708a78370c63ef67c436c15cabc9abb50
rr222cy <rr222cy@student.lnu.se>
2014-07-08 03:00:27 +0200

    New project for working towards DB with EF

---
+++ NextBirthday-EF/NextBirthday/Controllers/BirthdayController.cs
@@ -0,0 +1,46 @@
+﻿using NextBirthday.Models;
+using NextBirthday.Models.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Xml.Linq;
+
+namespace NextBirthday.Controllers
+{
+    public class BirthdayController : Controller
+    {
+        private XMLRepository _repository = new XMLRepository();
+
+        //
+        // GET: /Birthday/Index
+        public ActionResult Index()
+        {
+            return View("Index", _repository.GetBirthdays());
+        }
+
+        // GET: /Birthday/Create
+        public ActionResult Create()
+        {
+            return View("Create");
+        }
+
+        // POST: /Birthday/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Birthday birthday)
+        {
+            // Checking that correct values has been entered, and that the birthdate acctually is in the past.
+            if (ModelState.IsValid && birthday.Birthdate < DateTime.Today)
+            {
+                _repository.InsertBirthday(birthday);
+                _repository.Save();
+                return RedirectToAction("Index");
+            }
+
+            ModelState.AddModelError("Birthdate", "Your birthdate can't possibly be a date that has not passed!");
+            return View("Create", birthday);
+        }
+	}
+}
---
+++ NextBirthday-EF/NextBirthday/Models/Birthday.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace NextBirthday.Models
+{
+    public class Birthday
+    {
+        [Required(ErrorMessage="A birthdate must be entered.")]
+        [DisplayName("Birthdate")]
+        [DataType(DataType.Date)]
+        public DateTime Birthdate { get; set; }
+
+        [Required(ErrorMessage = "A name must be entered.")]
+        [DisplayName("Your name")]
+        public string Name { get; set; }
+
+        [ScaffoldColumn(false)]
+        public int Age
+        {
+            get
+            {
+                return this.NextBirthdayDate.Year - this.Birthdate.Year;
+            }
+        }
+
+        [ScaffoldColumn(false)]
+        public int DaysUntilNextBirthday
+        {
+            get
+            {
+                return (this.NextBirthdayDate - DateTime.Today).Days;
+            }
+        }
+
+        [ScaffoldColumn(false)]
+        public DateTime NextBirthdayDate
+        {
+            get
+            {
+                var nextBirthday = new DateTime(DateTime.Today.Year,
+                    this.Birthdate.Month, this.Birthdate.Day);
+                if (nextBirthday < DateTime.Today)
+                {
+                    nextBirthday = nextBirthday.AddYears(1);
+                }
+
+                return nextBirthday;
+            }
+        }
+    }
+}
